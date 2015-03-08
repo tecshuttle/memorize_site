818af8e5a393ced97a56c866639801fe177bfcf7
tom.xie <tecshuttle@gmail.com>
2015-03-08 12:28:03 +0800

    美化外观

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -1,7 +1,10 @@
 <div id="contents">
-    <p v-on="click: onNew" class="blog-item" style="text-align: center;">新建blog</p>
+    <div id="contents-list">
+        <p v-on="click: onNew" class="blog-item" style="text-align: center;">新建blog</p>
 
-    <p v-repeat="items" v-on="click: onClick" v-text="text | title" class="blog-item {{active ? 'active' : ''}}"></p>
+        <p v-repeat="items" v-on="click: onClick" v-text="text | title"
+           class="blog-item {{active ? 'active' : ''}}"></p>
+    </div>
 </div>
 
 <div id="blog">
@@ -10,22 +13,25 @@
         <span v-on="click: onDelete" class="btn">删除</span>
     </div>
 
-    <div class="preview">
+    <div class="blog-preview">
         <div v-html="text | marked" class="blog-content"></div>
     </div>
 </div>
 
 
 <div id="editor">
-
-
-    <div id="textarea">
-        <div class="edit-toolbar">
-            <span v-on="click: onCancel" class="btn">返回</span>
-            <span v-on="click: onSave" class="btn">保存</span>
-        </div>
-        <textarea v-model="input" class="form-control"></textarea>
+    <div class="edit-toolbar">
+        <span v-on="click: onCancel" class="btn">返回</span>
+        <span v-on="click: onSave" class="btn">保存</span>
     </div>
 
-    <div v-html="input | marked" class="preview"></div>
+    <div id="textarea">
+        <div id="textarea-wrap">
+            <textarea v-model="input" class="form-control"></textarea>
+        </div>
+    </div>
+
+    <div class="edit-preview">
+        <div v-html="input | marked" class="blog-content"></div>
+    </div>
 </div>
