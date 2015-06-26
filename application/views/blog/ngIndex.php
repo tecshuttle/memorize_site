192608aac2b337902423118d8d19ea55d55e7dc9
tecshuttle <tecshuttle@gmail.com>
2015-06-26 17:41:39 +0800

    ng blog 外观微调

--- application/views/blog/ngIndex.php
+++ application/views/blog/ngIndex.php
@@ -1,13 +1,15 @@
 <div ng-view></div>
 
 <script id="templates/home.html" type="text/ng-template">
-    <p ng-repeat="blog in blogs">
-        <a href="#blog/{{blog.cid}}">{{blog.title}} </a>
-    </p>
+    <div class="ng-blog">
+        <p ng-repeat="blog in blogs">
+            <a href="#blog/{{blog.cid}}">{{blog.title}} </a>
+        </p>
+    </div>
 </script>
 
 <script id="templates/blog.html" type="text/ng-template">
-    <div ng-bind-html="blog.text | markdown"></div>
+    <div ng-bind-html="blog.text | markdown" class="ng-blog"></div>
 </script>
 
 
