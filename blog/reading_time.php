bd1e73b538424706016d20bc3d06353dfdd73653
tecshuttle <tecshuttle@gmail.com>
2015-07-01 10:02:48 +0800

    [fixbug] blog无法统计阅读时间

--- blog/reading_time.php
+++ blog/reading_time.php
@@ -1,23 +1,17 @@
-<?php    
-require 'config.php'; 
-require 'functions.php'; 
+<?php
+require 'config.php';
+require 'functions.php';
 
-$uid = $_REQUEST['uid']; 
-$cid = $_REQUEST['cid']; 
-$sec = $_REQUEST['sec']; 
+$cid = $_REQUEST['cid'];
+$sec = $_REQUEST['sec'];
 
+$sql = "UPDATE contents SET readingTime = readingTime + $sec WHERE cid = $cid";
 
-$sql = "SELECT ID FROM reading_time WHERE cid = $cid AND uid = $uid";
-$rows = mysql_query($sql);
+$query = mysql_query($sql);
 
-if (mysql_num_rows($rows) > 0) {
-    $sql  = "UPDATE reading_time SET second = second + $sec WHERE cid = $cid AND uid = $uid";
-} else {
-    $sql  = "INSERT INTO reading_time (uid, cid, second) VALUES ($uid,$cid,$sec)";
-}
-
-//echo $sql;
-
-mysql_query($sql); 
+echo json_encode(array(
+    'success' => $query,
+    'sql' => $sql
+));
 
 //end file
